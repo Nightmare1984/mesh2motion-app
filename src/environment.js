43a583d032f44075de5022f5da711e11052e9a83
Scott Petrovic <scottpetrovic@gmail.com>
2025-11-02 10:51:27 -0600

    Just bring back a couple environment variables that might be useful

--- src/environment.js
+++ src/environment.js
@@ -1,4 +1,7 @@
 export const build_version = '0.0.0'
 
-// expose all Cloudflare environment variables to client
-window.PROCESS_ENV = PROCESS_ENV
+// grab a couple environment variables from Clourflare build process
+// Vite actually does the injection, so look at vite.config.js with the "define" block
+window.CLOUDFLARE_COMMIT_SHA = PROCESS_ENV.WORKERS_CI_COMMIT_SHA
+window.CLOUDFLARE_BRANCH = PROCESS_ENV.WORKERS_CI_BRANCH || 'unknown'
+
